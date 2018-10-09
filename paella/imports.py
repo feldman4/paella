3f928e0eebdaa00e131bf9c8f4ab29e76a2e5f4f
feldman <feldman4@mit.edu>
2018-10-09 13:23:59 -0400

    imports, constants

--- paella/imports.py
+++ paella/imports.py
@@ -1,5 +1,5 @@
 import regex as re
-from collections import Counter, OrderedDict
+from collections import Counter, OrderedDict, defaultdict
 import pandas as pd
 from Levenshtein import distance
 import numpy as np
@@ -10,6 +10,9 @@ from itertools import product, combinations
 from natsort import natsorted
 import os
 from glob import glob
+import functools
+from functools import partial
+
 
 sns.set(style='white', font_scale=1.5)
 
@@ -23,3 +26,24 @@ IPython.get_ipython().magic('autoreload 2')
 from paella import pairwise
 
 from paella.analysis import *
+
+from tqdm import tqdm
+
+FSC_A  = 'FSC_A'
+SSC_A  = 'SSC_A'
+FSC_H  = 'FSC_H'
+FSC_W  = 'FSC_Width'
+SSC_H  = 'SSC_H'
+FITC   = 'FITC_A'
+ECD    = 'ECD_A'
+TIME   = 'Time'
+
+### UTILS
+
+
+def bin_join(xs, symbol):
+    symbol = ' ' + symbol + ' ' 
+    return symbol.join('(%s)' % x for x in xs)
+        
+or_join  = functools.partial(bin_join, symbol='|')
+and_join = functools.partial(bin_join, symbol='&')
