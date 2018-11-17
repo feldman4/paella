2fe3428d83d951263bc3ad35ed2998d18e2b53fc
Unknown <fntsai@u.northwestern.edu>
2018-11-17 18:07:21 -0500

    add utils.py

---
+++ paella/utils.py
@@ -0,0 +1,18 @@
+import functools
+import pandas as pd
+
+
+def bin_join(xs, symbol):
+    symbol = ' ' + symbol + ' ' 
+    return symbol.join('(%s)' % x for x in xs)
+        
+or_join  = functools.partial(bin_join, symbol='|')
+and_join = functools.partial(bin_join, symbol='&')
+
+
+def set_apply(df, col, f):
+	"""Join the results of applying `f` (returns a dictionary).
+	"""
+	inputs = sorted(set(df[col]))
+	result = pd.DataFrame([f(x) for x in inputs], index=inputs)
+	return df.join(result, on=col)
